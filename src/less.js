f9c67deef06e23b8fd640e875e4157af6b96dd1e
qqqq4632 <qqqq4632@naver.com>
2020-09-27 15:13:29 +0900

    Enter

---
+++ src/less.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#00838f',
+        light: '#4fb3bf',
+        dark: '#005662',
+      },
+      secondary: {
+        main: '#0277bd',
+        light: '#58a5f0',
+        dark: '#004c8c',
+      },
+    },
+  });
+  
